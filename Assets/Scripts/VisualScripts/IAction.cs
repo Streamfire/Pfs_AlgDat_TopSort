1d93cf9417ed13cb541d8cf20b06f285d8aa677b
Conny Karras <CKextreme@users.noreply.github.com>
2018-01-14 18:18:41 +0100

    Create IAction.cs

---
+++ Assets/Scripts/VisualScripts/IAction.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+
+namespace VisualScripts
+{
+	/// <summary>
+	/// IAction Interface
+	/// </summary>
+	public interface IAction
+	{
+		// Used in AnimationQueue - the Method is inherited from the other Classes.
+		IEnumerator Run ();
+	}
+}
